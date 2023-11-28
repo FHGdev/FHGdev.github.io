d561950590211741e976fa5caf1b27104a3a01ad
Dennis Macapagal <macapagal.dennis@gmail.com>
2023-11-28 15:37:36 +0800

    fixes

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,33 +1,34 @@
-export const nav = [
+export const navGroup = [
 	{
 	  title: 'Home',
 	  slug: '/fhg/',
 	},
 	// {
 	//   title: 'About',
-	//   slug: '/fhg/about',
+	//   slug: '/t2delastro/about',
 	// },
 	{
-	  title: 'Blog',
-	  slug: '/fhg/blog',
-	//   submenu: [
-	// 	{
-	// 	  	title: 'First post',
-	// 	  	slug: '/fhg/blog/first-post',
-	// 	},
-	// 	{
-	// 		title: 'Second post',
-	// 		slug: '/fhg/blog/second-post',
-	// 	},
+	    title: 'Blog',
+	    slug: '/fhg/blog',
+	    submenu: [
+            {
+                title: 'First post',
+                slug: '/fhg/blog/first-post',
+            },
+            {
+                title: 'Second post',
+                slug: '/fhg/blog/second-post',
+            },
+        ]
 	// 	{
 	// 		title: 'Third post',
-	// 		slug: '/fhg/blog/third-post',
+	// 		slug: '/t2delastro/blog/third-post',
 	// 	},
 	//   ],
 	},
 	// {
 	//   title: 'Portfolio',
-	//   slug: '/fhg/portfolio',
+	//   slug: '/t2delastro/portfolio',
 	// },
 	{
 		title: 'Contact',
