14e88b48adde98f5cf477da02c0425ce2afe080f
FHGdev <136040730+FHGdev@users.noreply.github.com>
2024-06-04 14:22:38 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://FHGdev.github.io/',
-    base: '/fhg/',
+    base: '/',
     // output: 'server',
     // adapter: nodejs(),
 });
